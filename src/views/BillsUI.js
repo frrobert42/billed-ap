58a8fc62d090ea2f460ea5dc1b2886c21bec1f0a
Fred <robert.frederic7@gmail.com>
2024-04-12 14:01:32 +0200

    fix sort bills :bug:

--- src/views/BillsUI.js
+++ src/views/BillsUI.js
@@ -20,11 +20,15 @@ const row = (bill) => {
   }
 
 const rows = (data) => {
+    if (data && data.length) {
+        // Sort bills by date
+        data.sort((a, b) => (new Date(a.date) < new Date(b.date) ? 1 : -1));
+    }
   return (data && data.length) ? data.map(bill => row(bill)).join("") : ""
 }
 
 export default ({ data: bills, loading, error }) => {
-  
+
   const modal = () => (`
     <div class="modal fade" id="modaleFile" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
       <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
@@ -47,7 +51,7 @@ export default ({ data: bills, loading, error }) => {
   } else if (error) {
     return ErrorPage(error)
   }
-  
+
   return (`
     <div class='layout'>
       ${VerticalLayout(120)}
