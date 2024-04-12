7cd2fec80fe40aabfb3d2f3ffedb5efac49d1d2b
Fred <robert.frederic7@gmail.com>
2024-04-12 14:52:41 +0200

    add test Bills.js

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -5,16 +5,28 @@
 import {screen, waitFor} from "@testing-library/dom"
 import BillsUI from "../views/BillsUI.js"
 import { bills } from "../fixtures/bills.js"
-import { ROUTES_PATH} from "../constants/routes.js";
+import {ROUTES, ROUTES_PATH} from "../constants/routes.js";
 import {localStorageMock} from "../__mocks__/localStorage.js";
-
+import LoadingPage from "../views/LoadingPage.js";
+import ErrorPage from "../views/ErrorPage.js";
+import mockStore from "../__mocks__/store.js";
 import router from "../app/Router.js";
+import Bills from "../containers/Bills.js";
 
+const onNavigate = (pathname) => {
+  document.body.innerHTML = ROUTES({ pathname });
+};
+const newBills = new Bills({
+  document,
+  onNavigate,
+  store: mockStore,
+  localStorage: localStorageMock,
+});
 describe("Given I am connected as an employee", () => {
   describe("When I am on Bills Page", () => {
     test("Then bill icon in vertical layout should be highlighted", async () => {
 
-      Object.defineProperty(window, 'localStorage', { value: localStorageMock })
+      Object.defineProperty(window, 'localStorage', {value: localStorageMock})
       window.localStorage.setItem('user', JSON.stringify({
         type: 'Employee'
       }))
@@ -25,15 +37,69 @@ describe("Given I am connected as an employee", () => {
       window.onNavigate(ROUTES_PATH.Bills)
       await waitFor(() => screen.getByTestId('icon-window'))
       const windowIcon = screen.getByTestId('icon-window')
-      //to-do write expect expression
+      expect(windowIcon.classList.contains('active-icon')).toBeTruthy()
+    })
+    test("Then mail icon in vertical layout is not highlighted", async () => {
+
+      Object.defineProperty(window, 'localStorage', {value: localStorageMock})
+      window.localStorage.setItem('user', JSON.stringify({
+        type: 'Employee'
+      }))
+      const root = document.createElement("div")
+      root.setAttribute("id", "root")
+      document.body.append(root)
+      router()
+      window.onNavigate(ROUTES_PATH.Bills)
+      await waitFor(() => screen.getByTestId('icon-mail'))
+      const windowIcon = screen.getByTestId('icon-mail')
+      // check if the icon is highlighted
+      expect(!windowIcon.classList.contains('active-icon')).toBeTruthy()
 
     })
     test("Then bills should be ordered from earliest to latest", () => {
-      document.body.innerHTML = BillsUI({ data: bills })
+      document.body.innerHTML = BillsUI({data: bills})
       const dates = screen.getAllByText(/^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/i).map(a => a.innerHTML)
       const antiChrono = (a, b) => ((a < b) ? 1 : -1)
       const datesSorted = [...dates].sort(antiChrono)
       expect(dates).toEqual(datesSorted)
     })
+    test("Then bills should be displayed", () => {
+      document.body.innerHTML = BillsUI({data: bills})
+      const billsList = screen.getByTestId('tbody')
+      expect(billsList).toBeTruthy()
+      expect(billsList.children.length).toBe(bills.length)
+    })
+    test(" getBills should return bills", () => {
+      const bills = newBills.getBills();
+      expect(bills).toEqual(bills);
+    })
+    describe("When I click on NewBill button", () => {
+      test("Then it should render a Newbill page", () => {
+        document.body.innerHTML = BillsUI({data: []});
+        const handleClickNewBill = jest.fn(newBills.handleClickNewBill);
+        const button = screen.getByTestId("btn-new-bill");
+        button.addEventListener("click", handleClickNewBill);
+        button.click();
+        expect(handleClickNewBill).toBeCalled();
+        expect(screen.getByText("Envoyer une note de frais")).toBeTruthy();
+      });
+    });
+    describe("When Bills Page is loading", () => {
+      test("Then LoadingPage should be called", () => {
+        document.body.innerHTML = BillsUI({data: bills, loading: true});
+        expect(LoadingPage).toBeCalled;
+      });
+    });
+
+    describe("When Bills Page error", () => {
+      test("Then LoadingPage should be called", () => {
+        document.body.innerHTML = BillsUI({
+          data: bills,
+          loading: undefined,
+          error: true,
+        });
+        expect(ErrorPage()).toBeCalled;
+      });
+    });
   })
 })
--- src/containers/Bills.js
+++ src/containers/Bills.js
@@ -10,9 +10,7 @@ export default class {
     const buttonNewBill = document.querySelector(`button[data-testid="btn-new-bill"]`)
     if (buttonNewBill) buttonNewBill.addEventListener('click', this.handleClickNewBill)
     const iconEye = document.querySelectorAll(`div[data-testid="icon-eye"]`)
-    if (iconEye) iconEye.forEach(icon => {
-      icon.addEventListener('click', () => this.handleClickIconEye(icon))
-    })
+    if (iconEye) iconEye.forEach(icon => icon.addEventListener('click', () => this.handleClickIconEye(icon)))
     new Logout({ document, localStorage, onNavigate })
   }
 
@@ -29,12 +27,8 @@ export default class {
 
   getBills = () => {
     if (this.store) {
-      return this.store
-      .bills()
-      .list()
-      .then(snapshot => {
-        const bills = snapshot
-          .map(doc => {
+      return this.store.bills().list().then(snapshot => {
+        const bills = snapshot.map(doc => {
             try {
               return {
                 ...doc,
@@ -44,15 +38,9 @@ export default class {
             } catch(e) {
               // if for some reason, corrupted data was introduced, we manage here failing formatDate function
               // log the error and return unformatted date in that case
-              console.log(e,'for',doc)
-              return {
-                ...doc,
-                date: doc.date,
-                status: formatStatus(doc.status)
-              }
+              return {...doc, date: doc.date, status: formatStatus(doc.status)}
             }
           })
-          console.log('length', bills.length)
         return bills
       })
     }
